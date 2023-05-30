c6dc02fa3eaad45443c874f076e3670c02635e70
Václav Lux <pumlicek@gmail.com>
2023-05-30 20:33:42 +0200

    Adding lecture 37 and 38

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -11,13 +11,58 @@ public class LearnStrings {
 
     public static void main(String[] args) {
 
-        // Lecture 37 Searching within a String - indexOf()
-        String myText = "ABCDEFGABCDEFG";
+        String text = """
+                Orelia,Nore,2007-08-02,Serekunda,Grapefruit,NF
+                Marguerite,Teddman,2012-03-21,Lilongwe,Pear,BW
+                Roberta,Dom,2007-01-22,Salt Lake City,Grapefruit,ES
+                Jillayne,Thar,2001-10-21,Rockhampton,Pineapple,SK
+                Orelia,Ries,2014-09-03,Ulan Bator,Orrange,CR
+                """;
+
+        String[] people = text.split("\n");
+        System.out.println(people.length);
+        System.out.println(people[2]);
+        String[] orleia = people[2].split(",");
+        System.out.println(orleia.length);
+        System.out.println(orleia[3]);
+
+
+    }
+    //For lecture 37
+    /*public static String parseAreaCode(String phoneNumber) {
+        int openParens = phoneNumber.indexOf("(");
+        int closeParens = phoneNumber.indexOf(")");
+        String areaCode = phoneNumber.substring(openParens + 1, closeParens);
+        return areaCode;
+    }
+    public static String parseExchange(String phoneNumber) {
+        int spaceIndex = phoneNumber.indexOf(" ");
+        int dashIndex = phoneNumber.indexOf("-");
+        String exchange = phoneNumber.substring(spaceIndex + 1, dashIndex);
+        return exchange;
+    }
+    public static String parseLineNumber(String phoneNumber) {
+        int dashIndex = phoneNumber.indexOf("-");
+        String lineNumber = phoneNumber.substring(dashIndex + 1);
+        return lineNumber;
+    }*/
+}
+// Lecture 37 Searching within a String - indexOf()
+        /*String phoneNumber = "(234) 333-5551";
+
+        String areaCode = parseAreaCode(phoneNumber);
+        String exchange = parseExchange(phoneNumber);
+        String lineNumber = parseLineNumber(phoneNumber);
+
+        System.out.println(areaCode);
+        System.out.println(exchange);
+        System.out.println(lineNumber);*/
+        /* String myText = "ABCDEFGABCDEFG";
         System.out.println(myText.indexOf(65)); //or "A" case sensitive
         System.out.println(myText.lastIndexOf(65)); //or "A"
-        System.out.println(myText.lastIndexOf(65)); //or "A"
+        System.out.println(myText.indexOf(65, 2)); //or "A"*/
 
-        // Lecture 36 Getting part of the String
+// Lecture 36 Getting part of the String
         /* String myText = "apple";
         String firstPart = myText.substring(0, 1);
         String secondPart = myText.substring(1);
@@ -32,7 +77,7 @@ public class LearnStrings {
         System.out.println(myText);
         System.out.println(myNewText);
         System.out.println(firstLetterCapital);*/
-        // Lecture 35 Determining the Length of a String - length()
+// Lecture 35 Determining the Length of a String - length()
         /* Part One
         String myText = "for score and seven years ago";
         System.out.println(myText.length());
@@ -49,7 +94,7 @@ public class LearnStrings {
                 .append(secondText);
         System.out.println(builder.toString());*/
 
-        //Lecture 34 String Concatenation - concat
+//Lecture 34 String Concatenation - concat
         /* String text1 = "this is my text1";
         String text2 = "this is my text2";
         //System.out.println(text1 + " " + text2);
@@ -67,43 +112,43 @@ public class LearnStrings {
         String oneMoreFinalString = String.format("%s %s", text1, text2);
         System.out.println(oneMoreFinalString);*/
 
-        // Lecture 33 Determining if text is contained in String
+// Lecture 33 Determining if text is contained in String
         /*String myText = "Four score and seven years ago";
         System.out.println(myText.contains("seven"))*/
 
-        // lecture 32 Comparing String to alphabetic order part 1
+// lecture 32 Comparing String to alphabetic order part 1
         /* String firstWord = "Apple";
         String secondWord = "apple";
         System.out.println(firstWord.toUpperCase().compareTo(secondWord.toUpperCase()));
         System.out.println(compareToIgnoreCase(firstWord, secondWord));*/
 
-        //Lecture 31 Getting individual characters
+//Lecture 31 Getting individual characters
         /*String myText = "Apples";
         System.out.println(myText.charAt(3));*/
 
-        // Lecture 30 strip function
+// Lecture 30 strip function
         /*String firstName = "Jake";
         System.out.printf("'%s'", firstName.strip());//Strip*/
 
-        //29 Replacing Text within Strings
+//29 Replacing Text within Strings
         /* String myText = "My awesome text bitch";
         System.out.println(myText.replace('e','Z'));
         System.out.println(myText.replace("bitch","friend"));*/
 
-        //Lecture 28 Is String Blank or Empty
+//Lecture 28 Is String Blank or Empty
         /*String myText = "";
         System.out.println(myText.isEmpty());
         String myText2 = " ";
         System.out.println(myText2.isBlank());*/
 
-        //Lecture 27 Upper & lower casing
+//Lecture 27 Upper & lower casing
         /*String myText = "abcdefg";
         System.out.println(myText.toUpperCase());
 
         String myText2 = "ABCDEFG";
         System.out.println(myText2.toLowerCase());*/
 
-        //Lecture 26 creating strings
+//Lecture 26 creating strings
         /*String fruite = "apple";
         String anotherFruite = "apple";
         String vegetable = new String("broccoli");
@@ -111,5 +156,3 @@ public class LearnStrings {
 
         System.out.println(fruite == anotherFruite);
         System.out.println(vegetable == anotherVegetable);*/
-    }
-}
