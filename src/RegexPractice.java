5c5145e613845affba2ed7b5a13ebd4c02f7d37c
Václav Lux <pumlicek@gmail.com>
2023-06-04 22:17:26 +0200

    end ogf lecture 4

--- src/RegexPractice.java
+++ src/RegexPractice.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class RegexPractice {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPractice {
+
+    public static void main(String[] args) {
+        String phoneNumber = "1 (232) 333-3265";
+        String phoneNumberReg = """
+            # Regex for parsing Phone number
+            (?:(?<countryCode>\\d{1,3})?[-.,\\s]?)? # Get's Country Code
+            (?:\\(?(?<areaCode>\\d{3})\\)?[-.,\\s]?) # Get's Area Code
+            (?:(?<exchangeCode>\\d{3})[-.,\\s]?) # Get's Exchange Code
+            (?<lineNumber>\\d{3,4}) # Get's Line Number
+            """;
+
+        Pattern phoneNumPat = Pattern.compile(phoneNumberReg, Pattern.COMMENTS);
+        Matcher phoneNumberMat = phoneNumPat.matcher(phoneNumber);
+
+        if (phoneNumberMat.matches()){
+            System.out.format("Country code: %s\n",phoneNumberMat.group("countryCode"));
+            System.out.format("Area code: %s\n",phoneNumberMat.group("areaCode"));
+            System.out.format("Exchange code: %s\n",phoneNumberMat.group("exchangeCode"));
+            System.out.format("Line number code: %s\n",phoneNumberMat.group("lineNumber"));
+        } else {
+            System.out.println("We are F*cked");
+        }
+    }
 }
--- src/RegexPractice2.java
+++ src/RegexPractice2.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class RegexPractice2 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPractice2 {
+    public static void main(String[] args) {
+        String trascript = """
+                Student Number:	1234598872			Grade:		11
+                Birthdate:		01/02/2000			Gender:	M
+                State ID:		8923827123
+                                
+                Cumulative GPA (Weighted)		3.82
+                Cumulative GPA (Unweighted)	3.46
+                """;
+
+        String regex = """
+                Student\\sNumber:\\s(?<studentNumber>\\d{10})\\s* # Grab student number
+                Grade:\\s+(?<grade>\\d{1,2})\\n # Grab grade
+                Birthdate:\\s+(?<birthDate>(?<birthMonth>\\d{2})/(?<birthDay>\\d{2})/(?<birthYear>\\d{4}))\\s* # Grab birthdate
+                Gender:\\s+(?<gender>\\w+)\\b\\n # Grab gender
+                State\\sID:\\s+(?<stateID>\\d+)\\b #Grab StateID
+                Weighted\\)\\s+(?<weightedGPA>[\\d\\.]+)\\b.* #Grab Weighted GPA
+                Unweighted\\)\\s+(?<unweightGPA>[\\d\\.]+)\\b.* #Grab Unweighted GPA
+                """;
+        Pattern pat = Pattern.compile(regex, Pattern.DOTALL | Pattern.COMMENTS);
+        Matcher mat = pat.matcher(trascript);
+
+        if (mat.matches()){
+            System.out.println(mat.group("studentNumber"));
+            System.out.println(mat.group("grade"));
+            System.out.println(mat.group("birthMonth"));
+            System.out.println(mat.group("birthDay"));
+            System.out.println(mat.group("birthYear"));
+            System.out.println(mat.group("birthDate"));
+            System.out.println(mat.group("gender"));
+            System.out.println(mat.group("stateID"));
+            System.out.println(mat.group("weightedGPA"));
+            System.out.println(mat.group("unweightGPA"));
+        }
+    }
 }
